ca94ed937180d1d15695e8816484e87747c4d44f
Augusto Dwenger <hamburghammer@gmail.com>
2020-09-29 22:27:24 +0200

    Add first interaction with a sqlite db

--- main.go
+++ main.go
@@ -3,39 +3,124 @@ package main
 import (
 	"database/sql"
 	"fmt"
+	"log"
 
 	_ "github.com/mattn/go-sqlite3"
 )
+
+// TABLENAME name of the table for the application
+const TABLENAME = "tweet"
 
 func main() {
 	db, err := sql.Open("sqlite3", "./foo.db")
 	if err != nil {
 		fmt.Println(err)
 	}
-
 	defer db.Close()
-	createTable("test", db)
+
+	err = tableCleanup(db)
 	if err != nil {
 		fmt.Println(err)
+		return
+	}
+
+	err = createTable(db)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	err = insertInto(db)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	err = selectAll(db)
+	if err != nil {
+		fmt.Println(err)
+		return
 	}
 }
 
-func createTable(name string, db *sql.DB) error {
-	stmt, err := db.Prepare("CREATE TABLE ? ( 'id' INTEGER PRIMARY KEY AUTOINCREMENT, 'username' VARCHAR(64) NULL)")
+func createTable(db *sql.DB) error {
+	stmt, err := db.Prepare("CREATE TABLE IF NOT EXISTS tweet ( 'id' INTEGER PRIMARY KEY AUTOINCREMENT, 'username' VARCHAR(64) NULL)")
+	if err != nil {
+		return err
+	}
+	defer stmt.Close()
+
+	_, err = stmt.Exec()
 	if err != nil {
 		return err
 	}
 
-	res, err := stmt.Exec(name)
+	return nil
+}
+
+func insertInto(db *sql.DB) error {
+	usernames := []string{"admin", "user", "alice", "bob"}
+	stmt, err := db.Prepare("INSERT INTO tweet (username) VALUES (?)")
+	if err != nil {
+		return err
+	}
+	defer stmt.Close()
+
+	for _, username := range usernames {
+		_, err = stmt.Exec(username)
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func selectAll(db *sql.DB) error {
+	stmt, err := db.Prepare("SELECT * FROM tweet")
+	if err != nil {
+		return err
+	}
+	defer stmt.Close()
+
+	rows, err := stmt.Query()
+	if err != nil {
+		return err
+	}
+	defer rows.Close()
+
+	var (
+		id       int
+		username string
+	)
+	for rows.Next() {
+		err := rows.Scan(&id, &username)
+		if err != nil {
+			log.Println(err)
+		}
+
+		log.Printf("ID: %d, username: %s", id, username)
+	}
+	err = rows.Err()
 	if err != nil {
 		return err
 	}
 
-	rows, err := res.RowsAffected()
+	return nil
+
+}
+
+func tableCleanup(db *sql.DB) error {
+	stmt, err := db.Prepare("DROP TABLE IF EXISTS tweet")
+	if err != nil {
+		return err
+	}
+	defer stmt.Close()
+
+	_, err = stmt.Exec()
 	if err != nil {
 		return err
 	}
 
-	fmt.Println(rows)
 	return nil
 }
